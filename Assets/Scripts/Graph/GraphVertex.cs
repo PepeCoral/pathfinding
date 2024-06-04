51dd413c12ae3b09ec2adc75868d563a02b389c7
Pepe <coritochico@gmail.com>
2024-06-04 14:34:54 +0200

    fix(Graph): :bug: Corrected the getter and setter for value so it handles errors

--- Assets/Scripts/Graph/GraphVertex.cs
+++ Assets/Scripts/Graph/GraphVertex.cs
@@ -1,18 +1,26 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System;
 
 
 namespace pepe.graph
 {
     public class GraphVertex<T>
     {
-        public T Value { get; set; }
-        public List<GraphVertex<T>> Neighbors { get; set; }
+
+        private T _value;
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                _value = value ?? throw new ArgumentNullException(nameof(value), "Vertex value cannot be null");
+            }
+        }
+        public List<GraphVertex<T>> Neighbors { get; }
 
         public GraphVertex(T value)
         {
-            Value = value;
+            Value = value ?? throw new ArgumentNullException(nameof(value), "Vertex value cannot be null");
             Neighbors = new List<GraphVertex<T>>();
         }
     }
