086a3e16f9efb485a7510226d8de8036cb8b2e09
Pepe <coritochico@gmail.com>
2024-06-04 12:09:02 +0200

    feat(Character): :sparkles: Character moves to a given position
    Character moves to a given position in a straight line

--- Assets/Scripts/Character/CharacterMover.cs
+++ Assets/Scripts/Character/CharacterMover.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+namespace pathfinding.pepe.character
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CharacterMover : MonoBehaviour
     {
-        
-    }
+        [SerializeField] float walkingSpeed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 _postionToMove;
+
+
+        private void OnEnable()
+        {
+            CharacterBehaviour.OnPositionOrdered += MoveToPosition; 
+        }
+        private void OnDisable()
+        {
+            CharacterBehaviour.OnPositionOrdered -= MoveToPosition;
+        }
+        private void MoveToPosition(Vector3 position)
+        {   
+            //Corrected Position makes sure the Y value is the same height as the character
+            Vector3 correctedPosition = new Vector3(position.x, transform.position.y, position.z);
+            _postionToMove = correctedPosition;
+
+        }
+
+        private void FixedUpdate()
+        {
+            if (_postionToMove != null)
+            { 
+                transform.LookAt(_postionToMove);
+                transform.position+= transform.forward*walkingSpeed;
+            }
+        }
     }
 }
